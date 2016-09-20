9687b72120ab1ee13c5c041c62302107e9156c0d
Mike Cuddy <mcuddy77@gmail.com>
2016-09-20 09:24:29 -0400

    added a practice document with python shapes

--- prac.py
+++ prac.py
@@ -9,11 +9,25 @@ green = (0,255,0)
 blue = (0,0,255)
 
 gameDisplay = pygame.display.set_mode((800,600))
-
 gameDisplay.fill(black)
 
-while True:
 
+
+pixAr = pygame.PixelArray(gameDisplay)
+pixAr[10][20] = green
+
+pygame.draw.line(gameDisplay, blue, (100,200), (300,450), 5)
+pygame.draw.rect(gameDisplay, red, (400,400, 50,25) )
+pygame.draw.circle(gameDisplay, white, (150,150), 75)
+pygame.draw.polygon(gameDisplay, green, ((25,75), (76,125), (250,375), (400,25)))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    pygame.display.update()
 
 
 
