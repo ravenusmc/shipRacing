9aee588439d803ffd67b4f8a8e2decf008bdd7e5
Mike Cuddy <mcuddy77@gmail.com>
2016-10-14 13:51:26 -0400

    Added text to red button.

--- gamelearn.py
+++ gamelearn.py
@@ -55,6 +55,19 @@ def message_display(text):
 def crash():
     message_display("You Crashed!")
 
+def button(msg,x,y,w,h,ic,ac):
+        mouse = pygame.mouse.get_pos()
+        if x + w > mouse[0] > x and y + h > mouse[1] > y:
+            pygame.draw.rect(gameDisplay, ac, (x,y,w,h))
+        else:
+            pygame.draw.rect(gameDisplay, ic, (x,y,w,h))
+
+        smallText = pygame.font.Font("freesansbold.ttf", 20)
+        textSurf, textRect = text_objects(msg, smallText)
+        textRect.center = ((x+(w/2)), (y + (h/2)))
+        gameDisplay.blit(textSurf, textRect)
+        
+
 def game_intro():
     intro = True
     while intro:
@@ -68,19 +81,9 @@ def game_intro():
         TextSurf, TextRect = text_objects("A Bit Racey", largeText)
         TextRect.center = ((display_width/2), (display_height/2))
         gameDisplay.blit(TextSurf, TextRect)
-        
-        mouse = pygame.mouse.get_pos()
-        if 150 + 100 > mouse[0] > 150 and 450 + 50 > mouse[1] > 450:
-            pygame.draw.rect(gameDisplay, bright_green, (150,450,100,50))
-        else:
-            pygame.draw.rect(gameDisplay, green, (150,450,100,50))
 
-        smallText = pygame.font.Font("freesansbold.ttf", 20)
-        textSurf, textRect = text_objects("Go!", smallText)
-        textRect.center = ((150+(100/2)), (450 + (50/2)))
-        gameDisplay.blit(textSurf, textRect)
-
-        pygame.draw.rect(gameDisplay, red, (550,450,100,50))
+        button("Go!", 150,450,100,50, green, bright_green)
+        button("Quit!", 550,450,100,50, red, bright_red)
         
         pygame.display.update()
         clock.tick(15)
